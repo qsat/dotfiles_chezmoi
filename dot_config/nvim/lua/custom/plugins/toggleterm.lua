41c4cf9c6f2e4023d86895e7045b6b91a5334e04
r_arai_satoshi <s_arai@r.recruit.co.jp>
2026-03-11 00:01:25 +0900

    Update .config/nvim/lua/custom/plugins/toggleterm.lua

--- dot_config/nvim/lua/custom/plugins/toggleterm.lua
+++ dot_config/nvim/lua/custom/plugins/toggleterm.lua
@@ -21,6 +21,17 @@ return {
 				width = 80,
 				height = 80,
 			},
+			on_open = function(term)
+				-- ターミナルが開いた時に左端へ移動させる
+				vim.cmd("wincmd H")
+				vim.defer_fn(function()
+					local win_id = term.window
+					if vim.api.nvim_win_is_valid(win_id) then
+						vim.api.nvim_win_set_width(win_id, 80) -- ここで確実に幅を40にする
+						vim.api.nvim_win_set_option(win_id, "winfixwidth", true)
+					end
+				end, 10) -- 10ミリ秒待つ
+			end,
 		})
 	end,
 }
