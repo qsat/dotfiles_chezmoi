e9fb9c714c2f42a0ec75e9b46dc3fb5b4cab88f7
r_arai_satoshi <s_arai@r.recruit.co.jp>
2026-03-11 15:21:23 +0900

    Add .config/pm2/qwen3-coder/ecosystem.config.js

---
+++ dot_config/pm2/qwen3-coder/ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: "qwen3-server",
+      script: "llama-server",
+      args: [
+        "--model",
+        "~/.cache/huggingface/hub/models--unsloth--Qwen3-Coder-30B-A3B-Instruct-GGUF/snapshots/b17cb02dd882d5b6ab62fc777ad2995f19668350/Qwen3-Coder-30B-A3B-Instruct-Q4_K_M.gguf",
+        "--seed",
+        "3407",
+        "--temp",
+        "1.0",
+        "--top-p",
+        "0.95",
+        "--min-p",
+        "0.01",
+        "--top-k",
+        "40",
+        "--port",
+        "21434",
+        "--ctx-size",
+        "130672", // "65336",
+        "--n-gpu-layers",
+        "-1",
+      ].join(" "),
+      interpreter: "none", // バイナリを直接実行する場合
+    },
+  ],
+};
